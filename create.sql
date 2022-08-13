cd346eed0f8bddade2900d2d07019d154e206c57
Anton <58876946+KupriyanovAnton@users.noreply.github.com>
2022-08-13 14:39:00 +0400

    Update create.sql

--- create.sql
+++ create.sql
@@ -13,3 +13,23 @@ CREATE TABLE IF NOT EXISTS kupriyanov.operations_type(
 	name	varchar(8),
 	constraint operation_name_cstr CHECK(name IN ('withdraw', 'deposit'))
 );
+
+CREATE TABLE IF NOT EXISTS kupriyanov.documents_type(
+	code	serial PRIMARY KEY,
+	name	varchar(8)
+);
+
+CREATE TABLE IF NOT EXISTS kupriyanov.documents_type(
+	code	serial PRIMARY KEY,
+	name	varchar(8)
+);
+
+CREATE TABLE IF NOT EXISTS kupriyanov.documents(
+	code serial PRIMARY KEY,
+	doc_type integer CONSTRAINT doc_type_cstr REFERENCES kupriyanov.documents_type NOT NULL,
+	doc_series varchar(4) NOT NULL,
+	doc_number varchar(6) NOT NULL,
+	user REFERENCES users NOT NULL,
+	relevant boolean,
+	CONSTRAINT unique_docs_cstr UNIQUE(doc_type, doc_series, doc_number)
+);
