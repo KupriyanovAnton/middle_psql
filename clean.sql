cd72534bd30cb855f6dcf81f7a1dd8d6c745e18c
Anton <58876946+KupriyanovAnton@users.noreply.github.com>
2022-08-13 14:39:33 +0400

    Update clean.sql

--- clean.sql
+++ clean.sql
@@ -1,3 +1,5 @@
+DROP TABLE IF EXISTS kupriyanov.documents CASCADE;
+DROP TABLE IF EXISTS kupriyanov.documents_type CASCADE;
 DROP TABLE IF EXISTS kupriyanov.operations_type CASCADE;
 DROP TABLE IF EXISTS kupriyanov.currencies CASCADE;
 DROP TABLE IF EXISTS kupriyanov.users CASCADE;
