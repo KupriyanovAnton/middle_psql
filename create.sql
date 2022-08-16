f9e714df23368e887bb18b0f1141f8493716cd9c
Anton <58876946+KupriyanovAnton@users.noreply.github.com>
2022-08-16 22:36:18 +0400

    Update create.sql

--- create.sql
+++ create.sql
@@ -19,17 +19,29 @@ CREATE TABLE IF NOT EXISTS kupriyanov.documents_type(
 	name	varchar(8)
 );
 
-CREATE TABLE IF NOT EXISTS kupriyanov.documents_type(
-	code	serial PRIMARY KEY,
-	name	varchar(8)
-);
-
 CREATE TABLE IF NOT EXISTS kupriyanov.documents(
 	code serial PRIMARY KEY,
-	doc_type integer CONSTRAINT doc_type_cstr REFERENCES kupriyanov.documents_type NOT NULL,
+	doc_type_code integer CONSTRAINT doc_type_cstr REFERENCES kupriyanov.documents_type NOT NULL,
 	doc_series varchar(4) NOT NULL,
 	doc_number varchar(6) NOT NULL,
-	user REFERENCES users NOT NULL,
-	relevant boolean,
-	CONSTRAINT unique_docs_cstr UNIQUE(doc_type, doc_series, doc_number)
+	user_code integer REFERENCES kupriyanov.users NOT NULL,
+	relevant boolean NOT NULL,
+	CONSTRAINT unique_docs_cstr UNIQUE(doc_type_code, doc_series, doc_number)
+);
+
+CREATE TABLE IF NOT EXISTS kupriyanov.accounts(
+	code serial PRIMARY KEY,
+	acc_uuid UUID NOT NULL,
+	currencie_code varchar(3) REFERENCES kupriyanov.currencies NOT NULL,
+	user_code integer REFERENCES kupriyanov.users NOT NULL,
+	CONSTRAINT acc_code_currencie_unq UNIQUE(code, currencie_code)
+);
+
+CREATE TABLE IF NOT EXISTS kupriyanov.operations(
+	code serial PRIMARY KEY,
+	account_code integer NOT NULL, 
+	currencies_code varchar(3) NOT NULL, 
+	ammount numeric(10, 2), 
+	operations_type_code integer REFERENCES kupriyanov.operations_type NOT NULL,
+	CONSTRAINT account_currency_match_cstr FOREIGN KEY (account_code, currencies_code) REFERENCES kupriyanov.accounts(code, currencie_code) 
 );
